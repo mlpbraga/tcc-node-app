baec7e46bbf505cd0cefc56bd3bf923c8d3bb743
Luísa Braga <mlpb@icomp.ufam.edu.br>
2019-12-06 21:19:12 -0400

    fix: cors 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const errorHandler = require('./src/middlewares/error-handler');
 const { applyMiddlewares } = require('./src/middlewares');
 
@@ -14,12 +15,14 @@ const app = express();
 
 applyMiddlewares(app);
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   next();
+// });
+
+app.user(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
