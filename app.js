2fd2435457f38bf141a2423478bcfaa0490548bf
Luísa Braga <mlpb@icomp.ufam.edu.br>
2019-12-06 21:22:37 -0400

    fix

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const errorHandler = require('./src/middlewares/error-handler');
 const { applyMiddlewares } = require('./src/middlewares');
 
@@ -15,14 +14,12 @@ const app = express();
 
 applyMiddlewares(app);
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-//   next();
-// });
-
-app.user(cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
