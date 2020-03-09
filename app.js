71eeb36d0409c7ee07b6441e176248d31711ca71
Luísa Braga <mlpb@icomp.ufam.edu.br>
2020-03-08 22:05:24 -0400

    aaa

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ applyMiddlewares(app);
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  // res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
